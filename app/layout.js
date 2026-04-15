ee1ce8d05a1f5e1f409073fa06e18ee8ac2f4837
amoshsingh-14 <amosone4all@gmail.com>
2026-04-16 03:07:28 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Welth",
+  title: "FinWise",
   description: "One stop Finance Platform",
 };
 
